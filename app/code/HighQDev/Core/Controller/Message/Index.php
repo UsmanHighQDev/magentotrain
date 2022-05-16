55105861798ae99f6d6847fa72afd54b333a20d6
usman munir <usman.munir@highqdev.com>
2022-05-16 18:41:07 +0500

    add comments and showed success msg with validation

--- app/code/HighQDev/Core/Controller/Message/Index.php
+++ app/code/HighQDev/Core/Controller/Message/Index.php
@@ -16,12 +16,10 @@ class Index extends \Magento\Framework\App\Action\Action
     {
         $request = $this->getRequest();
         $data = $request->getParams();
-        if ($data['sku']) {
-
-            $this->messageManager->addError(__("Invalid SKU"));
-
+        if (array_key_exists("sku", $data) && $data['sku']) {
+            $this->messageManager->addSuccess(__("Your message has been saved against the product having SKU " . $data['sku']));
         } else {
-            $this->messageManager->addSuccess(__("Your message has been saved "));
+            $this->messageManager->addError(__("Invalid SKU"));
         }
         $resultRedirect = $this->resultRedirectFactory->create();
         $resultRedirect->setPath('core/form/index');
