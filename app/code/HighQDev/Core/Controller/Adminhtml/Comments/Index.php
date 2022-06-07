7e7fa8d48292d81385acc21fca2abfb53c2909e5
usman munir <usman.munir@highqdev.com>
2022-06-07 17:04:26 +0500

    tried for admin grid

---
+++ app/code/HighQDev/Core/Controller/Adminhtml/Comments/Index.php
@@ -0,0 +1,41 @@
+<?php
+declare(strict_types=1);
+
+namespace HighQDev\Core\Controller\Adminhtml\Comments;
+
+/**
+ * Class Index
+ * @package HighQDev\Core\Controller\Adminhtml\Post
+ */
+class Index extends \Magento\Backend\App\Action
+{
+    /**
+     * @var bool|\Magento\Framework\View\Result\PageFactory
+     */
+    protected $resultPageFactory = false;
+
+    /**
+     * Index constructor.
+     * @param \Magento\Backend\App\Action\Context $context
+     * @param \Magento\Framework\View\Result\PageFactory $resultPageFactory
+     */
+    public function __construct(
+        \Magento\Backend\App\Action\Context $context,
+        \Magento\Framework\View\Result\PageFactory $resultPageFactory
+    ) {
+        parent::__construct($context);
+        $this->resultPageFactory = $resultPageFactory;
+    }
+
+    /**
+     * @return \Magento\Framework\View\Result\Page
+     */
+    public function execute()
+    {
+        $resultPage = $this->resultPageFactory->create();
+        $resultPage->getConfig()->getTitle()->prepend((__('Comments')));
+        return $resultPage;
+    }
+
+
+}
