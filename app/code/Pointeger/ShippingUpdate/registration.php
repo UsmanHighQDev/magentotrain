ee1f557bb0514e43cd052a4a20927a80dd3b8533
usman <usman.munir@highqdev.com>
2022-08-05 14:51:42 +0500

    updated checkout page using module and theme

---
+++ app/code/Pointeger/ShippingUpdate/registration.php
@@ -0,0 +1,6 @@
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Pointeger_ShippingUpdate',
+    __DIR__
+);
---
+++ app/code/Pointeger/ShippingUpdate/view/frontend/requirejs-config.js
@@ -0,0 +1,6 @@
+var config = {
+    paths: {
+        //'Magento_Checkout/template/shipping': 'Pointeger_ShippingUpdate/template/shipping',
+        // 'Magento_Checkout/template/summary': 'Pointeger_ShippingUpdate/template/summary'
+    }
+};
